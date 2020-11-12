ff271e41156b05c27592eff2695552bceb514307
SerhiiLototskii <lototskiysergey@gmail.com>
2020-11-12 17:55:57 +0200

    tipes

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,54 +1,53 @@
 import React from "react";
 import {usersStateType} from "../redux/users-reduser";
+import userPhoto from "../../assets/images/user.png";
+import * as axios from 'axios'
 
 export type UsersType = {
     users: Array<usersStateType>
     follow: (id: number) => void
     unfollow: (id: number) => void
+    setUsers: (users:usersStateType) => void
 }
 
-let Users = (props: UsersType) => {
-/*
+class Users  extends React.Component<UsersType> {
 
-    if(props.users.length === 0) {
-        props.setUsers ( [
-            {id: 1,photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQUg0cFnYeMrjdJjXv7sBPvObKwhzU7LIv2DA&usqp=CAU",
-                followed: true, fullName: "Sergey", status: "im a boss", location: {city: 'Kiev', country: "Ukraine"}},
-            {id: 2,photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQUg0cFnYeMrjdJjXv7sBPvObKwhzU7LIv2DA&usqp=CAU",
-                followed: false, fullName: "Sania", status: "im a boss too", location: {city: 'Toronto', country: "Dubai"}},
-            {id: 3,photoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQUg0cFnYeMrjdJjXv7sBPvObKwhzU7LIv2DA&usqp=CAU",
-                followed: true, fullName: "Nikitos", status: "im a boss too", location: {city: 'Mirovoe', country: "Ukraine"}},
+    componentDidMount(): void {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
+    }
 
-
-        ])
+    render (){
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div key={u.id}>
+                            <div><img src={u.photos.small != null ? u.photos.small : userPhoto} alt=""/></div>
+                            <div>
+                                {u.followed
+                                    ? <button onClick={() => {
+                                        this.props.unfollow(u.id)
+                                    }}>Unfollow</button>
+                                    : <button onClick={() => {
+                                        this.props.follow(u.id)
+                                    }}>Follow</button>}
+                            </div>
+                            <div>
+                                <div>{u.fullName}</div>
+                                <div>{u.status}</div>
+                            </div>
+                            <div>
+                                <div>{u.location.country}</div>
+                                <div>{u.location.city}</div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+        )
+    }
 
     }
-*/
-
-
-    return (
-        <div>
-            {
-                props.users.map(u => <div key={u.id}>
-                        <div><img src={u.photoUrl} alt=""/></div>
-                        <div>
-                            {u.followed
-                                ?  <button onClick={() => {props.unfollow(u.id)} }>Unfollow</button>
-                            :<button onClick={() => {props.follow(u.id)} }>Follow</button>}
-                        </div>
-                        <div>
-                            <div>{u.fullName}</div>
-                            <div>{u.status}</div>
-                        </div>
-                    <div>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
-                    </div>
-                    </div>
-                )
-            }
-        </div>
-    )
-}
 
 export default Users
